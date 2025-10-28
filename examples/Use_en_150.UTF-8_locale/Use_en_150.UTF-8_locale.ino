45e543c987c3dbad4b4bb42bdeb0bd62c9a63cff
Bojan Jurca <42709677+BojanJurca@users.noreply.github.com>
2025-10-28 18:18:17 +0100

    Create Use_en_150.UTF-8_locale.ino

---
+++ examples/Use_en_150.UTF-8_locale/Use_en_150.UTF-8_locale.ino
@@ -0,0 +1,39 @@
+#include <locale.hpp>
+#include <console.hpp>
+
+
+void setup () {
+    cinit (); // three optional arguments: bool waitForSerial = false, unsigned int waitAfterSerial = 100 [ms], unsigned int serialSpeed = 115200 (9600 for AVR boards)
+
+    cout << fixed << setprecision (6) << showpoint;
+
+    cout << "Temperature of the Sun's photosphere is " << 9800 << " ℉\n";
+    cout << "Stefan-Boltzmann constant is " << 5.670374419 << " · 10⁻⁸ [W·m⁻²·K⁻⁴]\n";
+
+    #ifndef ARDUINO_ARCH_AVR
+        time_t currentTime = 1761600424;
+        struct tm structuredCurrentTime;
+        localtime_r (&currentTime, &structuredCurrentTime);
+        cout << "The time of writing this code is " << structuredCurrentTime << endl;
+    #endif
+
+
+    setlocale (lc_all, "en_150.UTF-8");
+
+
+    cout << "Temperature of the Sun's photosphere is " <<  5430 << " ℃\n";
+    cout << "Stefan-Boltzmann constant is " << 5.670374419 << " · 10⁻⁸ [W·m⁻²·K⁻⁴]\n";
+
+    cout << "Please enter π using en_150.UTF-8 locale: ";
+    double d;
+    cin >> d;
+    cout << d << ( abs (3.14159265359 - d) < 0.01 ? " is close enough\n" : " is not π\n" );
+
+    #ifndef ARDUINO_ARCH_AVR
+        cout << "The time of writing this code is " << structuredCurrentTime << endl;
+    #endif
+}
+
+void loop () {
+
+}
